fa6f764302aa9671f909252316a88a95df66273e
imfunny <36105478+imfunniee@users.noreply.github.com>
2018-11-21 18:14:21 +0530

    Add files via upload
    first commit, probably not last

---
+++ index.js
@@ -0,0 +1,46 @@
+const electron = require('electron');
+const path = require('path');
+const url = require('url');
+
+const {app, BrowserWindow, Menu} = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+mainWindow = new BrowserWindow({
+    frame: false, //fasle in production
+    icon: path.join(__dirname, 'img/icon.png'),
+});
+mainWindow.maximize();
+mainWindow.loadFile('index.html');
+mainWindow.on('closed', function(){
+    app.quit();
+});
+const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+Menu.setApplicationMenu(null); //null in production
+});
+
+//create menu
+const mainMenuTemplate = [];
+
+//mac thingy 
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'DevTools',
+        submenu:[
+            {
+            label: 'Toggle DevTools',
+            click(item, focusedWindow){
+                focusedWindow.toggleDevTools();
+            }
+         },
+         {
+            role: 'reload'
+         }
+        ]
+    });
+}
---
+++ main.js
@@ -0,0 +1,102 @@
+const $ = require('jQuery');
+const date = require('date-and-time');
+const wifiList = require('wifi-list-windows');
+const si = require('systeminformation');
+const vol = require('vol');
+ 
+vol.get().then((level)=> {
+    $("#level").html(Math.round(Number(level)*100));
+    $("#volumeRange").val(Math.round(Number(level)*100));
+});
+
+wifiList((err, networks) => {
+    if (err) throw err
+    if(networks.length !== 0){
+        showNetwork(networks);
+    }
+});
+
+function showNetwork(wifi){
+    for(i = 0; i < wifi.length; i++){
+        $("#wifi").append(`<div class="inner-wifi">${wifi[i].ssid}<br><span>${wifi[i].authentication} &nbsp; ${Math.round(Number(wifi[i].signal)*100)}%</span></div>`);
+    }
+}
+
+let now = new Date();
+date.setLocales('en', {
+    A: ['AM', 'PM']
+});
+var day = date.format(now, 'MMM DD YYYY');
+var time = date.format(now, 'hh:mm A');
+$("#time").html(`${time}<br>${day}`);
+setInterval(() => {
+let now = new Date();
+date.setLocales('en', {
+    A: ['AM', 'PM']
+});
+var day = date.format(now, 'MMM DD YYYY');
+var time = date.format(now, 'hh:mm A');
+$("#time").html(`${time}<br>${day}`);
+},30000);
+
+$("#task_wifi").click(() => {
+    $("#volume").addClass("animated zoomOutUp");
+    $("#wifi").css("display","inherit");
+    $("#wifi").addClass("animated zoomInDown");
+    setTimeout(() => {
+        $("#wifi").removeClass("animated zoomInDown");
+        $("#volume").removeClass("animated zoomOutUp");
+        $("#volume").css("display","none");
+    },800);
+});
+$("#task_vol").click(() => {
+    $("#wifi").addClass("animated zoomOutUp");
+    $("#volume").css("display","inherit");
+    $("#volume").addClass("animated zoomInDown");
+    setTimeout(() => {
+        $("#volume").removeClass("animated zoomInDown");
+        $("#wifi").css("display","none");
+        $("#wifi").removeClass("animated zoomOutUp");
+    },800);
+});
+//for now this is how it shows/hides but in future there will be close,maximize and minimise buttons
+function show_pro(){
+    $("#wifi").css("display","none");
+    $("#volume").css("display","none");
+    $("#processes").css("display", "inherit");
+    $("#processes").addClass("animated zoomInLeft");
+    setTimeout(() => {
+        $("#processes").removeClass("animated zoomInLeft");
+        $("#pro_btn").attr("onclick","hide_pro()");
+    },800);
+}
+function hide_pro() {
+    $("#wifi").css("display","none");
+    $("#volume").css("display","none");
+    $("#processes").addClass("animated zoomOutRight");
+    setTimeout(() => {
+        $("#processes").removeClass("animated zoomOutRight");
+        $("#pro_btn").attr("onclick","show_pro()");
+        $("#processes").css("display", "none");
+    },800);
+}
+//processes thingy ends here
+$("#volumeRange").on("change , mousemove", () => {
+    var nowVolume = $("#volumeRange").val();
+    vol.set(nowVolume/100);
+    $("#level").html(nowVolume);
+});
+
+si.processes().then((data) => {
+    $("#pro_number").html(`Processes (${data.all})`);
+    for(i = 0; i < data.list.length; i++){
+        $("#real_processes").append(`
+        <tr>
+            <td>${data.list[i].name}</td>
+            <td>${data.list[i].pid}</td>
+            <td>${data.list[i].priority}</td>
+            <td>${data.list[i].started}</td>
+        </tr>
+        `);
+    }
+});
